61f91d1be4cbe72edc54f2f42efba6d256b939cf
Guilherme Vieira <super.driver.512@gmail.com>
2014-03-24 22:38:58 -0300

    Add node test helpers and test input.

---
+++ jscss.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+/*
+    Copyright (c) 2013, 2014 Guilherme Vieira (super.driver.512@gmail.com)
+
+    This file is part of JSCSS.
+
+    JSCSS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    JSCSS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with JSCSS. If not, see <http://www.gnu.org/licenses/>.
+*/
+var fs = require('fs');
+var peg = require('pegjs');
+var input_file = process.argv[2];
+if(!input_file) {
+	console.error("Missing input file.");
+	console.log("Usage: node jscss.js input-file");
+	process.exit(-1);
+}
+var input_data = fs.readFileSync(input_file, 'utf8');
+var grammar_source = fs.readFileSync(__dirname + '/jscss.pegjs', 'utf8');
+var jscss;
+try {
+	jscss = peg.buildParser(grammar_source);
+}
+catch(err) {
+	console.error("Could not build parser:", err.message);
+	process.exit(-1);
+}
+var parse_results;
+try {
+	parse_results = jscss.parse(input_data);
+}
+catch(err) {
+	if(err.name !== 'SyntaxError') {
+		throw err;
+	}
+	console.error("Line", err.line + ", column", err.column + ":", err.message);
+	process.exit(-1);
+}
+console.dir(jscss.parse(input_data));
