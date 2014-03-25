d17b6a9373a97394df91fdff4491344fe4737a46
Guilherme Vieira <super.driver.512@gmail.com>
2014-03-25 02:01:19 -0300

    refactor: Move code generation logic into its own module.

---
+++ code-generator.js
@@ -0,0 +1,61 @@
+module.exports = function(ast) {
+	var code = 'var jscss$css = "";';
+	ast.forEach (
+		function(element) {
+			switch(element.type) {
+				case 'javascript':
+					code += '\n' + element.code + ';'
+					break;
+				case 'css':
+					var compute_locals = 'var locals = [];';
+					var next_local_id = 0;
+					var rule_body_parts = [];
+					var rule_body;
+					element.properties.forEach (
+						function(property) {
+							var value;
+							if(typeof(property.value) === 'string') {
+								value = JSON.stringify(property.value);
+							}
+							else {
+								value = property.value.map (
+									function(part) {
+										if(typeof(part) === 'string') {
+											return JSON.stringify(part);
+										}
+										else {
+											compute_locals +=
+													'\nlocals.push(eval('
+													+ JSON.stringify(part.js)
+													+ '));';
+											return 'locals[' + (next_local_id++) + ']';
+										}
+									}
+								);
+							}
+							rule_body_parts = rule_body_parts.concat (
+								JSON.stringify('\n\t' + property.name + ': ')
+								, value
+								, '";"'
+							);
+						}
+					);
+					rule_body = rule_body_parts.join('\n+ ');
+					code +=
+							'\n(function() {'
+							+ '\n' + compute_locals
+							+ '\njscss$css += '
+							+ JSON.stringify(element.selector)
+							+ '\n+ ' + JSON.stringify('\n{')
+							+ '\n+ ' + rule_body
+							+ '\n+ ' + JSON.stringify('\n}')
+							+ '\n})();';
+					break;
+				default:
+					throw new Error("Unknown element type: '" + element.type + "'.");
+			}
+		}
+	);
+	code += '\nreturn jscss$css;';
+	return code;
+}
--- jscss.js
+++ jscss.js
@@ -20,6 +20,7 @@
 var util = require('util');
 var fs = require('fs');
 var peg = require('pegjs');
+var codegen = require('./code-generator');
 var input_file = process.argv[2];
 if(!input_file) {
 	console.error("Missing input file.");
@@ -55,67 +56,9 @@ console.log (
 		}
 	)
 );
-var final_code = 'var jscss$css = "";';
-parse_results.forEach (
-	function(element) {
-		switch(element.type) {
-			case 'javascript':
-				final_code += '\n' + element.code + ';'
-				break;
-			case 'css':
-				var compute_locals = 'var locals = [];';
-				var next_local_id = 0;
-				var rule_body_parts = [];
-				var rule_body;
-				element.properties.forEach (
-					function(property) {
-						var value;
-						if(typeof(property.value) === 'string') {
-							value = JSON.stringify(property.value);
-						}
-						else {
-							value = property.value.map (
-								function(part) {
-									if(typeof(part) === 'string') {
-										return JSON.stringify(part);
-									}
-									else {
-										compute_locals +=
-												'\nlocals.push(eval('
-												+ JSON.stringify(part.js)
-												+ '));';
-										return 'locals[' + (next_local_id++) + ']';
-									}
-								}
-							);
-						}
-						rule_body_parts = rule_body_parts.concat (
-							JSON.stringify('\n\t' + property.name + ': ')
-							, value
-							, '";"'
-						);
-					}
-				);
-				rule_body = rule_body_parts.join('\n+ ');
-				final_code +=
-						'\n(function() {'
-						+ '\n' + compute_locals
-						+ '\njscss$css += '
-						+ JSON.stringify(element.selector)
-						+ '\n+ ' + JSON.stringify('\n{')
-						+ '\n+ ' + rule_body
-						+ '\n+ ' + JSON.stringify('\n}')
-						+ '\n})();';
-				break;
-			default:
-				throw new Error("Unknown element type: '" + element.type + "'.");
-		}
-	}
-);
-final_code += '\nreturn jscss$css;';
 console.log();
-console.log("Final JavaScript function body:");
-console.log(final_code);
+var final_code = codegen(parse_results);
+console.log("Final JavaScript function body:\n" + final_code);
 console.log();
 console.log("Final CSS:");
 console.log((new Function(final_code))());
